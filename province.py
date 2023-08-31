1b6eea07f6cfdf22bb5bb1e15f865603ef022a54
kingkelv20 <kelvinagimogim@gmail.com>
2023-08-31 02:46:44 -0700

    Organic_Direct

--- province.py
+++ province.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" a module that defines the City class """
+""" a module that defines the province class """
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ defines the city """
+    """ defines the province """
     state_id = ""
     name = ""
